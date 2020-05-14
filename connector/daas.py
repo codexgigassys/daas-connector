96b6e75911c8e8b1d5474d9ab3d4800577d86c32
Lucas <espositolucas95@gmail.com>
2020-05-14 17:37:05 -0300

    updated connector

--- connector/daas.py
+++ connector/daas.py
@@ -4,7 +4,7 @@ import socket
 import logging
 
 from env import envget
-from exceptions import MissingCredentialsException, InvalidCredentialsException
+from exceptions import MissingCredentialsException, InvalidCredentialsException, UnexpectedDaaSResponseException
 from utils import singleton
 
 
@@ -34,16 +34,16 @@ class DaaS(object):
         return self._request(url, method=requests.post, data=data)
 
     def _get(self, url):
-        return self._request(url, method=requests.post)
+        return self._request(url, method=requests.get)
 
     def _check_status_code(self, method_name, real_status_code, expected_status_code):
         error_message = 'status code of DaaS(...).%s(...) should be %s, but it is %s.' % (method_name, expected_status_code, real_status_code)
         assert real_status_code == expected_status_code, error_message
 
     def download_source_code(self, sample_sha1):
-        response = self._get('api/download_source_code/%s/' % sample_sha1).content
+        response = self._get('api/download_source_code/%s' % sample_sha1)
         self._check_status_code('download_source_code', response.status_code, 200)
-        return response
+        return response.content
 
     def send_sample_url(self, file_url, file_name, zip_password='codex', force_reprocess=False):
         data = {'file_url': file_url,
@@ -52,6 +52,28 @@ class DaaS(object):
                 'force_reprocess': force_reprocess}
         if self.callback_url:
             data['callback'] = self.callback_url
-        response = self._post('api/upload/', data)
+        response = self._post('api/upload', data)
         self._check_status_code('send_sample_url', response.status_code, 202)
         return response
+
+    def has_sample(self, hash):
+        response = self._get('api/get_sample_from_hash/%s' % hash)
+        if response.status_code == 200:
+            uploaded_to_daas = True
+        elif response.status_code == 404:
+            uploaded_to_daas = False
+        else:
+            raise UnexpectedDaaSResponseException()
+        return uploaded_to_daas
+
+    def sample_was_decompiled(self, hash):
+        response = self._get('api/get_sample_from_hash/%s' % hash)
+        logging.error('sample_was_decompiled(): response.status_code=%s  &  response.json()=%s' %
+                      (response.status_code, response.content))
+        if response.status_code == 200:
+            decompiled = response.json()['decompiled'] if 'decompiled' in response.json() else False
+        elif response.status_code == 404:
+            decompiled = False
+        else:
+            raise UnexpectedDaaSResponseException()
+        return decompiled
